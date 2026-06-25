74b5829089ea2e7a3fe1e6c1d165c52cd2682f4e
Albert A. <albert@mybizniche.com>
2026-06-25 10:59:08 +0800

    Add Handwritten Reviews Page Pattern with Content and Custom Fields

---
+++ template-parts/page-patterns/handwritten-reviews.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Page Pattern: Handwritten Reviews
+ *
+ * This file contains the complete page data for the 'Handwritten Reviews' page.
+ * It can be imported to create/update the page on other environments.
+ *
+ * Includes: Content, Featured Image, Status, Attributes, Custom Fields
+ *
+ * To use: Tools → Page Content Sync → Import All Pages from Files
+ *
+ * @package CustomTheme
+ */
+
+return array(
+	'title'               => 'Handwritten Reviews',
+	'slug'                => 'handwritten-reviews',
+	'status'              => 'publish',
+	'excerpt'             => '',
+	'parent_slug'         => '',
+	'menu_order'          => 0,
+	'template'            => '',
+	'featured_image_url'  => '',
+	'featured_image_path' => '', // Theme assets path (ships via Git)
+	'custom_fields'       => array(),
+	'content'             => <<<'EOD'
+<!-- wp:mbn-theme/hero-section {"eyebrowText":"Reviews","mainHeading":"Handwritten Reviews","subheading":"","videoMp4Url":"http://hastingsandhastings.dev.local/wp-content/uploads/2026/06/vid-arizona-highway-at-dusk.mp4","videoMp4Id":10061,"videoWebmUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2026/06/vid-arizona-highway-at-dusk.webm","videoWebmId":10060,"overlayImageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2026/06/img-fg-video-overlay-1.webp","overlayImageId":9912,"showCtaBar":false,"contentJustify":"justify-between","contentVerticalAlign":"items-end","textMaxWidth":"max-w-3xl","showQuickCall":true} /-->
+
+<!-- wp:mbn-theme/handwritten-reviews {"accordionItems":[{"id":"mqs41drzyve7rm7ubx","heading":"March 2022","images":[{"id":"mqs42irokeorg5k05l","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2022/03/1.jpg","imageId":10683},{"id":"mqs5vkimkynbzrdhv9","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2022/03/2.jpg","imageId":10684},{"id":"mqs5vojfdkryvgn4cl","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2022/03/3.jpg","imageId":10685},{"id":"mqs5vtgwwe82lixc3w","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2022/03/4.jpg","imageId":10686}]},{"id":"mqs5y6kycopsicn8so5","heading":"Januarry 2022","images":[{"id":"mqs5ye28qqg4nporqsc","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2022/01/20220120103408325.jpg","imageId":10680},{"id":"mqs5yo78n30ltu6qyrb","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2022/01/20220120103413807-1.jpg","imageId":10681},{"id":"mqs5yss1e0cc5juntj8","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2022/01/20220120103413807-2.jpg","imageId":10682}]},{"id":"mqs5z9a82leuhgfwmuw","heading":"May 2021","images":[{"id":"mqs5zey0sx9fjbhn4i","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/05/may-1.jpg","imageId":10677},{"id":"mqs5zk1iyop19ymln2","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/05/may-2.jpg","imageId":10678},{"id":"mqs5znvzy2lo1c1bew","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/05/may-3.jpg","imageId":10679}]},{"id":"mqs62j59ftnsgs1n767","heading":"April 2021","images":[{"id":"mqs62qakhhw5uchx75v","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/april-1.jpg","imageId":10655},{"id":"mqs650kg2jychzmm6ih","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/april-2.jpg","imageId":10656},{"id":"mqs655d9665t5n0kfah","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/april-3.jpg","imageId":10657},{"id":"mqs65a202j79kmizs8t","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/april-4.jpg","imageId":10658},{"id":"mqs65don5a2wj3ewrs","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/april-5.jpg","imageId":10659}]},{"id":"mqs65r8wtxbn2eyqcc","heading":"February 2021","images":[{"id":"mqs65zla3gciqn8hwqu","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-1.jpg","imageId":10660},{"id":"mqs66cifa6uo54z6h6v","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-2.jpg","imageId":10669},{"id":"mqs66igd3nanpvtihmt","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-3.jpg","imageId":10670},{"id":"mqs66lh0l5gjdq3195","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-4.jpg","imageId":10671},{"id":"mqs66s703zef8jx5qjg","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-5.jpg","imageId":10672},{"id":"mqs66x1puz3amz3pdfp","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-6.jpg","imageId":10673},{"id":"mqs6716itgegd7muqa","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-7.jpg","imageId":10674},{"id":"mqs674yknrlfb42kqpk","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-8.jpg","imageId":10675},{"id":"mqs67912fhcjy1jc6x","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-9.jpg","imageId":10676},{"id":"mqs67d9m6wrfnlo7k6","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-10.jpg","imageId":10661},{"id":"mqs67l5qjzvyut5pmm","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-11.jpg","imageId":10662},{"id":"mqs67pzwq21ubi5fu7h","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-12.jpg","imageId":10664},{"id":"mqs67tgcheiahut57k","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-13.jpg","imageId":10665},{"id":"mqs67x4ezk5tp7xw2ta","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-14.jpg","imageId":10666},{"id":"mqs683xso2cxgi6qvk9","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-15.jpg","imageId":10667},{"id":"mqs687nrph8cbdscqna","imageUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2021/04/feb-16.jpg","imageId":10668}]}]} /-->
+
+<!-- wp:mbn-theme/section-video-bg-form {"videoMp4Url":"http://hastingsandhastings.dev.local/wp-content/uploads/2026/06/vid-breathtaking-and-beautiful-view-of-phoenix-az.mp4","videoWebmUrl":"http://hastingsandhastings.dev.local/wp-content/uploads/2026/06/vid-breathtaking-and-beautiful-view-of-phoenix-az.webm","gravityFormId":"1","align":"full"} /-->
+EOD
+	,
+);
